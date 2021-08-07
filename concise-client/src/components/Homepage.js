4a63c79ed7d4e73c781940d69f9a446c3861fc18
Nishi Anand <nishianand.05@gmail.com>
2021-08-07 06:37:44 +0000

    Add login and signup forms

---
+++ concise-client/src/components/Homepage.js
@@ -0,0 +1,18 @@
+import React from "react";
+import {Link} from "react-router-dom";
+import homeVideo from "../images/homeVideo.mp4"
+
+const Homepage = () => (
+	<div>
+		<video playsInline autoPlay muted loop id="myHomeVideo"><source src={homeVideo} type="video/mp4"/></video>
+		<div className="home-hero">	
+			<h1>What's Happening</h1>
+			<h4>New to Concise?</h4>
+			<Link to="/signup" className="btn btn-primary">
+			Sign up here
+			</Link>
+		</div>
+	</div>
+)
+
+export default Homepage;
--- concise-client/src/containers/App.js
+++ concise-client/src/containers/App.js
@@ -4,6 +4,8 @@ import {configureStore} from "../store";
 import {BrowserRouter as Router} from "react-router-dom";
 
 import Navbar from './Navbar';
+import Main from './Main';
+
 
 const store = configureStore();
 
@@ -12,6 +14,7 @@ const App = () => (
 		<Router>
 			<div className="onboarding">
 				<Navbar/>
+				<Main/>
 			</div>
 		</Router>
 	</Provider>
